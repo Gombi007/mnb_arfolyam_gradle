f59012b705b451ef83a961417a68c5cc829c1aa7
Gombi007 <gombos88@gmail.com>
2021-06-30 21:27:07 +0200

    java classes from wsdl check

---
+++ src/main/java/com/greenfox/arfolyam/ArfolyamApplication.java
@@ -0,0 +1,13 @@
+package com.greenfox.arfolyam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArfolyamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArfolyamApplication.class, args);
+    }
+
+}
